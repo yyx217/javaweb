de6435067aa1b269a1467c032ae41dab414716b6
yyx <yyx217@163.com>
2018-09-01 19:51:33 +0800

    创建maven项目

---
+++ src/main/java/cn/edu/swufe/javaweb/utils/DbUtils.java
@@ -0,0 +1,23 @@
+package cn.edu.swufe.javaweb.utils;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class DbUtils {
+private static BasicDataSource dataSource;
+	
+	static {		
+		dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://192.168.137.1/swufe");
+		dataSource.setUsername("root");
+		dataSource.setPassword("yyx191702");		
+	}
+	
+	
+	public static DataSource getDataSource() {
+		
+		return dataSource;
+	}
+}
