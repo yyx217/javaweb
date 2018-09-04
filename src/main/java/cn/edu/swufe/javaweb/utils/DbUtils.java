a976d70eb9b7c6db1e3369fb481816c2b45234ea
major721 <2294268338@qq.com>
2018-09-04 10:29:13 +0800

    src

--- src/main/java/cn/edu/swufe/javaweb/utils/DbUtils.java
+++ src/main/java/cn/edu/swufe/javaweb/utils/DbUtils.java
@@ -10,7 +10,11 @@ private static BasicDataSource dataSource;
 	static {		
 		dataSource = new BasicDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+<<<<<<< HEAD
 		dataSource.setUrl("jdbc:mysql://192.168.48.84:3306/javaweb");
+=======
+		dataSource.setUrl("jdbc:mysql://192.168.48.84/javaweb");
+>>>>>>> a669c582b784430f1b38a33091a49a766f98f3eb
 		dataSource.setUsername("root");
 		dataSource.setPassword("yyx191702");	
 	}
